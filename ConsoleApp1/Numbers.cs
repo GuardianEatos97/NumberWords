d899b9e1e2797161d9171ce36eb346cbb16cc96d
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2023-12-08 03:11:41 +0200

    First Commit

---
+++ ConsoleApp1/Numbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordNumbers
+{
+    internal class Numbers
+    {
+        public static string WordNums(int num)
+        {
+            string[] singledigits = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",};
+            string[] smalldualdigits = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+            string[] tensstring = {"", "",  "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+            if (num > 0 & num <10)
+            {
+                return singledigits[num];
+            }
+            else if (num == 0) 
+            {
+                return "zero";
+            }
+
+            else if (num >= 10 & num <20) 
+            {
+                return smalldualdigits[num-10];
+            }
+
+            else if (num >= 20 & num < 100)
+            {
+                return tensstring[num / 10] + " " + singledigits[num % 10]; 
+            }
+
+            else if (num >= 100 & num <= 999) 
+            {
+                if (num % 10 == 0)
+                {
+                    
+                }
+                return singledigits[num/100] + " hundred" + " " + tensstring[num%100/10] + " " + tensstring[num%10];
+
+                
+            }
+
+            return "";
+        }
+        
+    }
+    
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace WordNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type A 4 Digit Number");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Numbers.WordNums(numbers));
+        }
+    }
+}
